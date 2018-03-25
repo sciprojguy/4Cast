f5dd2236aa9fc12da80b4f61d4b834f78ff89dc4
Chris Woodard <sciprojguy@gmail.com>
2018-03-25 14:22:29 -0400

    more things the cherry pick missed

--- 4Cast/Forecast/ForecastCell.m
+++ 4Cast/Forecast/ForecastCell.m
@@ -21,8 +21,8 @@
     self.iconView.image = [[IconCache sharedCache] iconNamed:item.icon];
     self.descriptionLabel.text = item.weatherDescription;
     
-    self.lowTempLabel.text = [[NSString alloc] initWithFormat:@"Low: %.1f ºF", [item.tempMin doubleValue]];
-    self.highTempLabel.text = [[NSString alloc] initWithFormat:@"High: %.1f ºF", [item.tempMax doubleValue]];
+    self.lowTempLabel.text = [[NSString alloc] initWithFormat:@"Low: %.1f ºF", item.tempMin];
+    self.highTempLabel.text = [[NSString alloc] initWithFormat:@"High: %.1f ºF", item.tempMax];
     NSDateFormatter *fmtr = [[NSDateFormatter alloc] init];
     fmtr.dateFormat = @"MMM d yy hh:mm a";
     self.dateAndTime.text = [fmtr stringFromDate:item.dateTime];
