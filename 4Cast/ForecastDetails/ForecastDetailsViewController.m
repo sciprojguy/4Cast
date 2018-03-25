5063e7ae09a4161c474b968b66e8c315d6a9618f
Chris Woodard <sciprojguy@gmail.com>
2018-03-25 14:22:04 -0400

    things the cherry pick missed

--- 4Cast/ForecastDetails/ForecastDetailsViewController.m
+++ 4Cast/ForecastDetails/ForecastDetailsViewController.m
@@ -42,10 +42,10 @@
     self.iconView.image = iconImg;
     self.descriptionLabel.text = item.weatherDescription;
     
-    self.lowTempLabel.text = [[NSString alloc] initWithFormat:@"Low of %.1f ºF", [item.tempMin doubleValue]];
-    self.highTempLabel.text = [[NSString alloc] initWithFormat:@"High of %.1f ºF", [item.tempMax doubleValue]];;
+    self.lowTempLabel.text = [[NSString alloc] initWithFormat:@"Low of %.1f ºF", item.tempMin];
+    self.highTempLabel.text = [[NSString alloc] initWithFormat:@"High of %.1f ºF", item.tempMax];
     
-    self.humidityLabel.text = [[NSString alloc] initWithFormat:@"Rel Humidity %@%%", item.humidity];
+    self.humidityLabel.text = [[NSString alloc] initWithFormat:@"Rel Humidity %ld%%", item.humidity];
 }
 
 - (void)didReceiveMemoryWarning {
