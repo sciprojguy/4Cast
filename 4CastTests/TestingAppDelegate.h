2e9b703c5b3033416d48499d8ec2e6834ded36c2
Chris Woodard <sciprojguy@gmail.com>
2018-04-13 09:43:30 -0400

    modification for TestingAppDelegate (https://qualitycoding.org/ios-app-delegate-testing/)

---
+++ 4CastTests/TestingAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TestingAppDelegate.h
+//  4CastTests
+//
+//  Created by Chris Woodard on 4/13/18.
+//  Copyright © 2018 SampleSoft. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TestingAppDelegate : NSObject
+@property (nonatomic, strong) UIWindow *window;
+@end
---
+++ 4CastTests/TestingAppDelegate.m
@@ -0,0 +1,12 @@
+//
+//  TestingAppDelegate.m
+//  4CastTests
+//
+//  Created by Chris Woodard on 4/13/18.
+//  Copyright © 2018 SampleSoft. All rights reserved.
+//
+
+#import "TestingAppDelegate.h"
+
+@implementation TestingAppDelegate
+@end
